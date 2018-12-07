d869f9abb3f7e2c78137873f3bacaa92789ddb18
Daniel Cazzulino <daniel@cazzulino.com>
2018-12-07 02:09:11 -0300

    Properly exit the console with error code
    This change allows the console to exist cleanly when the
    web app failed to start if the ports aren't available.
    
    We don't implement all of the Ctrl+C/Close behavior as
    explained in https://github.com/dotnet/coreclr/issues/8565#issuecomment-435698397
    because simply canceling the token is enough for our
    purposes.

--- wsrelay/Program.cs
+++ wsrelay/Program.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Hosting;
 
 namespace wsrelay
 {
     public class Program
     {
-        public static void Main(string[] args) => 
-            CreateWebHostBuilder(args).Build().Run();
+        public static async Task Main(string[] args)
+        {
+            using (var shutdown = new CancellationTokenSource())
+            {
+                try
+                {
+                    await CreateWebHostBuilder(args).Build().RunAsync(shutdown.Token);
+                }
+                catch (IOException io) when (io.InnerException is AddressInUseException aiu)
+                {
+                    shutdown.Cancel();
+                    Environment.Exit(-1);
+                }
+            }
+        }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
